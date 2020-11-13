e6981659474b7f12da4590053cbc6cc1d18673e2
Bobreg <chel006@rambler.ru>
2020-11-13 09:25:17 +0300

    исправление ошибки

--- myserial.cpp
+++ myserial.cpp
@@ -53,6 +53,7 @@ void MySerial::check_data(){
     crc += (unsigned char)arr[arr.length() - 2];
     if(crc == check_crc && arr[0] == (char)0x76 && (arr[1] == (char)0x77 || arr[1] == (char)0x78)){
         arr.remove(0, 3);
+        arr.remove(arr.length() - 2, 2);
         emit message(arr);
     }else{
         qDebug() << crc << check_crc;// << arr.toHex(':');
